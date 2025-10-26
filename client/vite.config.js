48480a7778904e0d5c452aadd59a5d9d18712304
samar-raghav1 <samarraghav001@gmail.com>
2025-10-26 08:03:58 +0530

    client updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,21 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import dotenv from "dotenv";
+
+dotenv.config();
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base:'./',
+  server:{
+    proxy:{
+      '/api':{
+        target:process.env.VITE_BASE_URL,
+        changeOrigin:true,
+        secure:true
+      }
+    }
+  }
 })
